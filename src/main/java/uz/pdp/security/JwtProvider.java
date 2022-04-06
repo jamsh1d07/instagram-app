e0ed3aee7858a2c5fd53137b7993fde976dda8e6
jamsh1d07 <raimovjamshid0704@gmail.com>
2022-04-06 22:25:04 +0500

    login kirish tekshirildi

--- src/main/java/uz/pdp/security/JwtProvider.java
+++ src/main/java/uz/pdp/security/JwtProvider.java
@@ -2,25 +2,38 @@ package uz.pdp.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import uz.pdp.entity.User;
+import uz.pdp.repository.UserRepository;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Component
 public class JwtProvider {
+    @Autowired
+    UserRepository userRepository;
     @Value("${app.jwt.secret}")
     String secretKey ;
     @Value("${app.jwt.expireTime}")
     long expire;
 
     public String generateToken(String username) {
-        return Jwts.builder()
-                .signWith(SignatureAlgorithm.HS512,secretKey)
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expire))
-                .compact();
+
+        Optional<User> byUserName = userRepository.findByUserName(username);
+        if (byUserName.isPresent()){
+            return Jwts.builder()
+                    .signWith(SignatureAlgorithm.HS512,secretKey)
+                    .setSubject(username)
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + expire))
+                    .compact();
+        }
+        return "Bunday username li foydalanuvchi yo'q";
+
     }
 
    public String getUserNameFromToken(String token){
